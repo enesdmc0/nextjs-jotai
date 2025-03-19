b2813ed735acdca57efe240d6c2cc498465d676f
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-03-20 00:00:06 +0300

    5: add jotai-devtools for enhanced state management debugging

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,16 @@
 // app/providers.jsx
 "use client";
 import { Provider } from "jotai";
-
+import { DevTools } from "jotai-devtools";
+import "jotai-devtools/styles.css";
 interface Props {
   children: React.ReactNode;
 }
 
 export function JotaiProvider({ children }: Props) {
-  return <Provider>{children}</Provider>;
+  return (
+    <Provider>
+      {children} <DevTools />
+    </Provider>
+  );
 }
