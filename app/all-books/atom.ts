51c2d6172404aaa888508236ddaa34d8b4fecd51
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-03-26 01:20:36 +0300

    9: refactor: remove unused state management for books; simplify AllBooks component

--- app/all-books/atom.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "jotai";
-
-export const tableDataAtom = atom([]);
-
-export const tableDataFetchAtom = atom(null, async (_, set) => {
-    const response = await fetch("http://localhost:3000/api/books");
-    const data = await response.json();
-    set(tableDataAtom, data);
-});
